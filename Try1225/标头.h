a6cf4e1cd3d8e692f6de342299aaeef57bca2f70
itair <itair@163.com>
2013-12-26 19:24:03 +0800

    第六章

---
+++ Try1225/标头.h
@@ -0,0 +1,4 @@
+#pragma once
+
+//TODO 加入一些声明与其他库
+#include <tchar.h>
---
+++ Try1225/源.cpp
@@ -0,0 +1,22 @@
+﻿//C++ Primer Plus 课后习题 p63 3.7 
+// by itAir
+// Try to use Google C++ programing style                                      
+#include "标头.h"
+#include <iostream>
+using namespace std;
+
+void main(){
+	cout<<"Hello World!"<<endl;
+	cout<<"___________________________________\n\n";
+	//1
+
+
+
+
+
+
+	system("pause");
+
+}
+
+
